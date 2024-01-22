5d7a97d4ec99b8550a7ea7204a2a5b1d687389f8
LeonardoLaurindo <137720280+LeonardoLaurindo@users.noreply.github.com>
2024-01-22 20:21:44 -0300

    template and bootstrap start

--- webapp/src/utils/templates.go
+++ webapp/src/utils/templates.go
@@ -10,6 +10,7 @@ var templates *template.Template
 // Insere os templates html na variavel templetes
 func CarregarTemplates() {
 	templates = template.Must(template.ParseGlob("views/*.html"))
+	templates = template.Must(templates.ParseGlob("views/templates/*.html"))
 }
 
 // Renderiza uma pagina html na tela
